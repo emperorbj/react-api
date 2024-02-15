a76c3ba7213f1ae2664c7ad898690c7166452bac
BOLAJI OPATOLA <138551605+emperorbj@users.noreply.github.com>
2024-02-14 17:56:29 -0800

    Update Product.jsx

--- src/Product.jsx
+++ src/Product.jsx
@@ -10,10 +10,7 @@ const Product = () => {
 
     //  // 1. use effect is called every rendering and rerendering
     // // 2. if you pass a second parameter as a blank array in the use effect call back function
-    // // it will only render once the first time. That array is called DEPENDENCY ARRAY( because we can pass dependencies in them)
-    // // 3. variables called within the dependency array renders the use effect call back
-    // // 4. if we want to do anything when a component unmounts we put another function in the
-    
+   
 
     
 
